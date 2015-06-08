9dbe2d5383fcdbedb08b9c4784ead64193f5d4ba
janosfarkas <farkas48@uniba.sk>
2015-06-08 13:59:02 +0200

    bugfix

--- ckanext/api_management/management.py
+++ ckanext/api_management/management.py
@@ -11,6 +11,7 @@ import ckan.lib.helpers as h
 import ckan.lib.navl.dictization_functions as df
 import ckan.plugins as p
 from ckan.common import _, c, g
+#import ckan.lib.app_globals.Globals as g
 import ckan.plugins.toolkit as toolkit
 import json
 import time
@@ -21,8 +22,8 @@ import ckan.logic
 import __builtin__
 import datetime
 def apikey_exist(apikey):
-    user = model.Session.query(model.User).filter(model.User.apikey == apikey).first()
-    return user != None
+	user = model.Session.query(model.User).filter(model.User.apikey == apikey).first()
+	return user != None
 class APIManagementController(base.BaseController):
     def _setup_template_variables(self, context, data_dict):
         try:
@@ -39,6 +40,8 @@ class APIManagementController(base.BaseController):
         context = {'model': model, 'session': model.Session,
                    'user': c.user or c.author, 'auth_user_obj': c.userobj,
                    'for_view': True}
+        if c.userobj == None:
+            base.abort(401, base._('Not authorized to see this page'))
         data_dict = {"id":c.userobj.id}
 
         self._setup_template_variables(context, data_dict)
@@ -59,8 +62,8 @@ class APIManagementController(base.BaseController):
         if len(from_) == 0:
             return base.render("API_management/api_management.html") 
         else:
-            
-            return h.redirect_to(from_.encode('ascii'))
+        	
+        	return h.redirect_to(from_.encode('ascii'))
 def is_installed():
     return True
 
